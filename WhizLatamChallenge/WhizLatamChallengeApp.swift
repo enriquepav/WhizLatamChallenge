11a9f3aa7d93f4c4660d70d9177b25590cc5f5e8
Enrique Alata <e.alatavences@gmail.com>
2023-04-30 09:20:45 -0500

    Initial Commit

---
+++ WhizLatamChallenge/WhizLatamChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhizLatamChallengeApp.swift
+//  WhizLatamChallenge
+//
+//  Created by MAC1DIGITAL20 on 30/04/23.
+//
+
+import SwiftUI
+
+@main
+struct WhizLatamChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
